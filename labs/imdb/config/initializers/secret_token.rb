f6f8bb106803f7bd9159f1d2874fff0a63f549c0
Zahra Jabini <zahra.arch@gmail.com>
2012-11-12 13:38:23 -0500

    beginner rails app

---
+++ labs/imdb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imdb::Application.config.secret_token = '34ce233504a2fdfcba3c414d9d3368c60695e0cd5ed24a3ebf136afc84a8ca147a057ad2ffa72876e21ce8338cb538ce88d600ebcb70b3e3d972fd9c0bfbf5e9'
