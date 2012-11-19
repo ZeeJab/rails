7058c4ad400a5be6116a50b69638b8673c11c333
Zahra Jabini <zahra.arch@gmail.com>
2012-11-19 13:27:16 -0500

    nothing to do with muzik scaffold added

---
+++ labs/muzikdemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzikdemo::Application.config.secret_token = '975755d25ff06c830c20adfc8f5e255b7fb71c5338265ed832bed61ba7927e55d1c68b4c3b5a383da54ff4366b90472e113638f135451cbb428359a6c3e442b4'
