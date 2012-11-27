d46f1f1a4da211b7ee60559b04efc632aa5c2ff4
Zahra Jabini <zahra.arch@gmail.com>
2012-11-27 14:49:48 -0500

    new labs

---
+++ labs/mythology/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mythology::Application.config.secret_token = '889a93ea00ef9bc9c9ed57c63d99b85f21ceb4aab3bc908354dd52f2d72da1cc28e28a35d8991d4fe8f8281478e6334091d0e3f1aa322eca5af91a1b75e9919c'
