98d8b17aebdfe27b5c4944a183ec5d98583ccf64
Zahra Jabini <zahra.arch@gmail.com>
2012-11-28 16:31:28 -0500

    geolocation map

---
+++ labs/vacationr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vacationr::Application.config.secret_token = '16a623f4c5a2430c670997ae7c4390c8eed9abe2b85497d2329c452b82ef3a6d1812d904e0e7d54815d04125296865903d8050617f7dcc67c599fcab64cad937'
