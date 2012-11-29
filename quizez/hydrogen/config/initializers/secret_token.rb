d2569e5ee9c7530f0402d8a60730f0e57385615d
Zahra Jabini <zahra.arch@gmail.com>
2012-11-29 10:23:07 -0500

    bootstrap quiz done

---
+++ quizez/hydrogen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydrogen::Application.config.secret_token = '19baef270d0a70906cafb866bc1e2b85fac0ba8b6ed46d81510d8806c909e32c7332d0afd415f4c78df33f9830ede4c009596b8e36b5e35b9f3c6c21635ac804'
