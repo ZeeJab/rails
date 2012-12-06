4b41c813ad6e16f2f91283f4fa7072667b3cf3cb
Zahra Jabini <zahra.arch@gmail.com>
2012-12-06 09:35:49 -0500

    quiz

---
+++ quizez/berylium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berylium::Application.config.secret_token = '44a28530fda1b2de4e79b0ca0561b47c18234dd1b37b4d28b95accdbd90b8838a2cf7937aa9a355dd3b076cc8a765457c3006f420b49feb7935f8163a3bdcd33'
