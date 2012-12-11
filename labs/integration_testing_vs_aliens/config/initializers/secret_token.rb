8f4c4e2bb1ef4e41a9ba72cc2b19749fc5fb3172
Zahra Jabini <zahra.arch@gmail.com>
2012-12-11 14:55:49 -0500

    testing

---
+++ labs/integration_testing_vs_aliens/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationTestingVsAliens::Application.config.secret_token = 'b4dc78627e545fdbe9d249a15a212cd47266176bab56a2acd35e5b3578035a8db2840d1c706b72e7bf5f8134089efa40f44a90b9674f613651ce9ca8378f3ab4'
